b9332b417ee8b4c989cb406926c75d3e19cb2001
Adrianmc94 <amiguezcampos@danielcastelao.org>
2026-01-08 09:45:46 +0100

    creacion e implementacion del archivo Record

--- app/src/main/java/com/example/simon_dice/data/local/Record.kt
+++ app/src/main/java/com/example/simon_dice/data/local/Record.kt
@@ -1,4 +1,16 @@
 package com.example.simon_dice.data.local
 
-class Record {
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "record_table")
+data class Record(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val score: Int,
+    val timestamp: Long
+) {
+    /**
+     * Lógica para comprobar si una nueva puntuación supera a la actual.
+     */
+    fun isNewRecord(finalScore: Int): Boolean = finalScore > score
 }
